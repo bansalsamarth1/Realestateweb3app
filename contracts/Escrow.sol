ed7e8e1aac04a9a1faeda012dc1f6ff0a4fe2a28
Samarth Bansal <samarthbansal_23me237@dtu.ac.in>
2024-08-25 16:58:16 +0530

    completed the lisitng property added only seller modifier

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -14,6 +14,13 @@ contract Escrow {
     address payable public seller;
     address public inspector;
     address public lender;
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+
+
     mapping(uint256=> bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
@@ -27,11 +34,15 @@ contract Escrow {
     }
 
 
-    function list(uint256 _nftID) public {
+    function list(uint256 _nftID ,address _buyer, uint256 _purchasePrice,uint256 _escrowAmount ) public payable onlySeller{
         //transfer the nft from seller to this contract
         IERC721(nftAddress).transferFrom(msg.sender,address(this), _nftID);
 
         isListed[_nftID] = true;
+        purchasePrice[_nftID]= _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+
         
     }
 }
